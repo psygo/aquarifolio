e900199f413313491ce69225327e326da6dca8c4
Philippe Fanaro <philippefanaro@gmail.com>
2024-05-28 15:37:19 -0300

    shad ui and daisyui together

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@shad"
+
 export default function Portfolio() {
   return (
-    <main className="light">
+    <main className="light min-h-screen">
       <h1>Portfolio</h1>
       <button className="btn btn-primary">Button</button>
+      <Button>Here</Button>
     </main>
   )
 }
--- tasks.md
+++ tasks.md
@@ -1,6 +1,5 @@
 # Tasks
 
-1. [ ] Overlay Link
 1. [ ] Timeline
 1. [ ] Projects List
 1. [ ] Contact Form
